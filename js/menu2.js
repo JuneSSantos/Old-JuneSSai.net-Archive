7083d1cbf7d977c2b96a3a56cfb5627e47f6f87a
June S. Sai <91368271+JuneSSai@users.noreply.github.com>
2022-09-02 07:57:02 -0300

    Add files via upload

---
+++ js/menu2.js
@@ -0,0 +1,35 @@
+let headerContent = `
+<a href="../index.html">
+    <img src="../images/buttons/homebutton.png" class="button"></a>
+
+  <a href="../blog/blogmain.html">
+    <img src="../images/buttons/blogbutton.png" class="button"></a>
+
+  <a href="../art/artmain.html">
+    <img src="../images/buttons/artbutton.png" class="button"></a>
+
+  <a href="../fanart/fanartmain.html">
+    <img src="../images/buttons/fanartbutton.png" class="button"></a>
+
+  <a href="../partners/partnersmain.html">
+    <img src="../images/buttons/partnersbutton.png" class="button"></a>
+    
+  <a href="../guestbook/guestbook.html">
+    <img src="../images/buttons/guestbookbutton.png" class="button"></a>
+
+  <a href="../servers/serversmain.html">
+    <img src="../images/buttons/serversbutton.png" class="button"></a>
+    
+  <a href="../downloads/downloadsmain.html">
+    <img src="../images/buttons/downloadsbutton.png" class="button"></a>
+    
+  <a href="../extras/extrasmain.html">
+    <img src="../images/buttons/extrasbutton.png" class="button"></a>
+
+  <a href="../donate/donatemain.html">
+    <img src="../images/buttons/donatebutton.png" class="button"></a>
+  
+  <a href="../about/about.html">
+    <img src="../images/buttons/aboutbutton.png" class="button"></a>
+`;
+document.querySelector('#buttoncontainer').insertAdjacentHTML('beforeend', headerContent);
